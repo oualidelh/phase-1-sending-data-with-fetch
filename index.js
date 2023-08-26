e2d3c77e99766c61ebbc1d5af6c532a4fff8c9af
oualidelhouari <elhouarioualid@gmail.com>
2023-08-27 00:08:02 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function renderUserId(outPut) {
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    h1.textContent = `${outPut}`;
+    div.appendChild(h1);
+    document.querySelector('body').appendChild(div);
+}
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        }),
+    }).then(resp => resp.json())
+        .then(user => renderUserId(user.id))
+        .catch(err => renderUserId(err.message));
+}
+submitData("Oualid", "Oualid@email.ih");
